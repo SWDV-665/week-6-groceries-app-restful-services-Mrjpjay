7cdb9c2c866a3c9ec61195a7413dcf9d481d7d51
Mrjpjay <mrjpjay2@gmail.com>
2023-07-21 10:34:51 -0400

    Week 3

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
@@ -7,8 +8,66 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
-  constructor(public navCtrl: NavController) {
+  items = [
+    {
+      name: "Milk",
+      quantity: 3
+    },
 
+    {
+      name: "Water",
+      quantity: 2
+    },
+
+    {
+      name: "Sugar",
+      quantity: 1
+    },
+  ]
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+
+  }
+
+  removeItem(item, i){
+    this.items.splice(i,1)
+  }
+  
+  addItem(){
+    console.log("adding item...");
+    this.showAddItemPrompt();
+  }
+
+  showAddItemPrompt() {
+    const prompt = this.alertCtrl.create({
+      title: 'Login',
+      message: "Enter a name for this new album you're so keen on adding",
+      inputs: [
+        {
+          name: 'name',
+          placeholder: 'Name'
+        },
+        {
+          name: 'quantity',
+          placeholder: 'Quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: item => {
+            this.items.push(item);
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
